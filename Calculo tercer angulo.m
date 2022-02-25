faf6a425e82afb7c19292c7ae6f6e8c08e651bb0
SuperD01 <93230898+SuperD01@users.noreply.github.com>
2022-02-24 23:41:25 -0600

    Add files via upload

---
+++ Calculo tercer angulo.m
@@ -0,0 +1,5 @@
+a = input('Inserte ángulo en grados: a = '); fprintf('\n');
+b = input('Inserte ángulo en grados: b = '); fprintf('\n');
+
+c = 180 - (a + b);
+fprintf('Ángulo en grados: c = %.2f\n\n', c);
